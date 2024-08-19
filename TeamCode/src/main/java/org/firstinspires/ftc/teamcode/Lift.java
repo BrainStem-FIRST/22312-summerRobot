7e004c5304261edc8b3733f314870e66a87b0bdb
kc0817 <kevinjkchen17@gmail.com>
2024-08-19 12:20:54 -0400

    Added enum states to lift and collector

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift.java
@@ -15,6 +15,16 @@ public class Lift extends Subsystem {
     DcMotorEx liftMotor;
     DcMotorEx collectMotor;
 
+    enum LiftStates {
+        UP, DOWN, STATIC
+    };
+    enum CollectorStates {
+        ON, OFF
+    };
+
+    LiftStates liftState;
+    CollectorStates collectorState;
+
     public Lift(HardwareMap hwMap, Telemetry telemetry, Gamepad gamepad1) {
         super(hwMap, telemetry, gamepad1);
 
@@ -24,20 +34,81 @@ public class Lift extends Subsystem {
         liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         collectMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        liftState = LiftStates.STATIC;
+        collectorState = CollectorStates.OFF;
+
+    }
+
+    private void turnCollecterOn() {
+        collectMotor.setPower(-gamepad1.right_trigger * 0.5);
+    }
+    private void turnCollectorOff() {
+        collectMotor.setPower(0);
+    }
+
+    private void raiseLift() {
+        liftMotor.setPower(0.3);
+    }
+    private void dropLift() {
+        liftMotor.setPower(-0.3);
+    }
+    private void holdLift() {
+        liftMotor.setPower(0);
+    }
+
+    private void updateLiftState() {
+        switch(liftState) {
+            case UP:
+                raiseLift();
+                break;
+            case DOWN:
+                dropLift();
+                break;
+            case STATIC:
+                holdLift();
+                break;
+        }
+    }
+    private void updateCollectorState() {
+        switch(collectorState) {
+            case ON:
+                turnCollecterOn();
+                break;
+            case OFF:
+                turnCollectorOff();
+                break;
+        }
+    }
+
+    private void checkLiftControls() {
+        if(gamepad1.a) {
+            liftState = LiftStates.UP;
+        } else if(gamepad1.b) {
+            liftState = LiftStates.DOWN;
+        } else {
+            liftState = LiftStates.STATIC;
+        }
+    }
+    private void checkCollectorControls() {
+        if(gamepad1.right_trigger > 0.2) {
+            collectorState = CollectorStates.ON;
+        } else {
+            collectorState = CollectorStates.OFF;
+        }
+    }
+
+    private void checkNewStates(){
+        checkLiftControls();
+        checkCollectorControls();
+    }
+
+    private void updateStates() {
+        updateLiftState();
+        updateCollectorState();
     }
 
     public void update() {
-        liftMotor.setPower(0);
-        if (gamepad1.b) {
-            liftMotor.setPower(0.3);
-        }
-        else if (gamepad1.a) {
-            liftMotor.setPower(-0.3);
-        }
-
-        collectMotor.setPower(0);
-        if (gamepad1.right_trigger > 0.2) {
-            collectMotor.setPower(-gamepad1.right_trigger * 0.5);
-        }
+        checkNewStates();
+        updateStates();
     }
 }
